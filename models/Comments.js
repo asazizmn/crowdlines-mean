36e22ac687166dd8da634da83154f7164349fa4a
Aziz <aziz.saboor@nexchange.com>
2015-09-02 11:20:59 +0800

    - updated the comments schema, since there was a missing mongoose reference

--- models/Comments.js
+++ models/Comments.js
@@ -6,20 +6,19 @@
  */
 
 
+// import global 'mongoose' object
+var mongoose = require( 'mongoose' );
+
 // define new comment schema
 // and add it to the global mongoose object
-require( 'mongoose' ).model
-( 
-    'Comment', 
-    new mongoose.Schema
-    ({
-        body: String,
-        author: String,
-        upvotes: { type: Number, default: 0 },
-            
-        // please note that we are makeing a reference back to the 'Post' objecyt form here
-        post: { type: mongoose.Schema.Types.ObjectId, ref: 'Post' }
-    }) 
-);
+mongoose.model( 'Comment', new mongoose.Schema
+({
+    body: String,
+    author: String,
+    upvotes: { type: Number, default: 0 },
+
+    // please note that we are makeing a reference back to the 'Post' objecyt form here
+    post: { type: mongoose.Schema.Types.ObjectId, ref: 'Post' }
+}));
 
 
